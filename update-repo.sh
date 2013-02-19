5256e105ff7ff5921f5a7d916f30a4276be64258
Karfield Chen <karfield@126.com>
2013-02-19 10:11:15 +0800

    Fix update-repo.sh cannot sync because of 'repo' missing
    Signed-off-by: Karfield Chen <karfield@126.com>

--- update-repo.sh
+++ update-repo.sh
@@ -29,22 +29,42 @@ else
     do_help=1
 fi
 
-wrapper_version=`cat $REPO_PATH/repo|grep "^VERSION *= *(.*)"|sed -n 's/.*(\([0-9]*\), *\([0-9]*\)).*/\1.\2/p'`
+IFS=$'\n'
+
+repo_info=`grep -E "^REPO_URL|^REPO_REV|^VERSION" $REPO_PATH/repo`
+for l in $repo_info; do
+    if test ${l:0:1} == "V"; then
+        wrapper_version=`echo $l|sed -n 's/.*(\([0-9]*\), *\([0-9]*\)).*/\1.\2/p'`
+    else
+        eval $l
+    fi
+done
+test -z $REPO_URL &&
+    REPO_URL='https://gerrit.googlesource.com/git-repo'
+test -z $REPO_REV &&
+    REPO_REV='stable'
+test -z $wrapper_version &&
+    wrapper_version=1.18
+
+if test ! -e $repo_dir/repo/.git; then
+    echo "Sync repo"
+    git clone $REPO_URL $repo_dir/repo
+fi
+
 repo_main() {
-    main_py=$REPO_PATH/main.py
     test -z $repo_dir &&
         repo_dir=./
-    exec $main_py --repo-dir=$repo_dir --wrapper-version=$wrapper_version \
-        --wrapper-path=$REPO_PATH -- $*
+    exec $REPO_PATH/main.py --repo-dir=$repo_dir \
+        --wrapper-version=$wrapper_version \
+        --wrapper-path=$repo_dir/repo \
+        -- $*
 }
 
 helps=`repo_main help init`
-_ifs=$IFS
-IFS=$'\n'
 for l in $helps; do
     if test ${#l} -gt 5 && test "${l:0:4}" == "    "; then
         repo_init_helps=$repo_init_helps$l$'\n'
-        IFS=$_ifs
+        IFS=$' '
         for w in $l; do
             case $w in
                 -*)
@@ -55,7 +75,7 @@ for l in $helps; do
         IFS=$'\n'
     fi
 done
-IFS=$_ifs
+IFS=$' '
 
 print_help() {
     cmd=`basename $0`
@@ -125,11 +145,11 @@ if test ! -e $repo_dir/manifests/.git/HEAD; then
         echo "No permission to create $repo_dir" &&
         exit 0
     echo "Start to init repostory..."
-    repo_main init $init_opt
+    repo_main init $init_opt --repo-url=$REPO_URL --repo-branch=$REPO_REV
 fi
 
 if test -e $repo_dir/manifests/.git/HEAD; then
     echo "Start to sync repo via network..."
-    repo_main sync --network-only -j $njobs $no_clone_bundle
+    repo_main sync --network-only -j $njobs $no_clone_bundle --no-repo-verify
 fi
 
